a7704347a7b42f347afb947dabb9de84d2a54a60
neiros <sergey.kopnov@gmail.com>
2017-12-01 23:11:59 +0700

    addition

---
+++ src/Lyra2RE/Lyra2RE.h
@@ -0,0 +1,14 @@
+#ifndef LYRA2RE_H
+#define LYRA2RE_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void lyra2re2_hashTX(const char* input, char* output, int len);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
