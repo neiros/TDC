116be62473cc8b84f8cd52aa9e08758e00b8246f
neiros <sergey.kopnov@gmail.com>
2018-01-23 23:28:27 +0700

    tBlock

--- src/rpcrawtransaction.cpp
+++ src/rpcrawtransaction.cpp
@@ -79,6 +79,7 @@ void TxToJSON(const CTransaction& tx, const uint256 hashBlock, Object& entry)
     entry.push_back(Pair("txid", tx.GetHash().GetHex()));
     entry.push_back(Pair("version", tx.nVersion));
     entry.push_back(Pair("locktime", (boost::int64_t)tx.nLockTime));
+    entry.push_back(Pair("tBlock", tx.tBlock));
     Array vin;
     BOOST_FOREACH(const CTxIn& txin, tx.vin)
     {
