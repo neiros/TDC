498343389daa3d565dbeed3ba2de6222f0ea5224
neiros <sergey.kopnov@gmail.com>
2020-04-28 11:59:56 +0700

    tdcoin test https://github.com/neiros/TDC/issues/7

--- src/test/Checkpoints_tests.cpp
+++ src/test/Checkpoints_tests.cpp
@@ -14,21 +14,21 @@ BOOST_AUTO_TEST_SUITE(Checkpoints_tests)
 
 BOOST_AUTO_TEST_CASE(sanity)
 {
-    uint256 p11111 = uint256("0x0000000069e244f73d78e8fd29ba2fd2ed618bd6fa2ee92559f542fdb26e7c1d");
-    uint256 p134444 = uint256("0x00000000000005b12ffd4cd315cd34ffd4a594f430ac814c91184a0d42d2b0fe");
-    BOOST_CHECK(Checkpoints::CheckBlock(11111, p11111));
-    BOOST_CHECK(Checkpoints::CheckBlock(134444, p134444));
+    uint256 p20000 = uint256("0x0000003bcec9382c0cc9c59871fc41f64f085194a5fa36853d9d98c592b58d52");
+    uint256 p222222 = uint256("0x000000034ac37950fc67fc42bb5c2e83479ff237f931d5e275ebc11ff9e236de");
+    BOOST_CHECK(Checkpoints::CheckBlock(20000, p20000));
+    BOOST_CHECK(Checkpoints::CheckBlock(222222, p222222));
 
     
     // Wrong hashes at checkpoints should fail:
-    BOOST_CHECK(!Checkpoints::CheckBlock(11111, p134444));
-    BOOST_CHECK(!Checkpoints::CheckBlock(134444, p11111));
+    BOOST_CHECK(!Checkpoints::CheckBlock(20000, p222222));
+    BOOST_CHECK(!Checkpoints::CheckBlock(222222, p20000));
 
     // ... but any hash not at a checkpoint should succeed:
-    BOOST_CHECK(Checkpoints::CheckBlock(11111+1, p134444));
-    BOOST_CHECK(Checkpoints::CheckBlock(134444+1, p11111));
+    BOOST_CHECK(Checkpoints::CheckBlock(20000+1, p222222));
+    BOOST_CHECK(Checkpoints::CheckBlock(222222+1, p20000));
 
-    BOOST_CHECK(Checkpoints::GetTotalBlocksEstimate() >= 134444);
+    BOOST_CHECK(Checkpoints::GetTotalBlocksEstimate() >= 222222);
 }    
 
 BOOST_AUTO_TEST_SUITE_END()
